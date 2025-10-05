96482da52d8c3d0a73cda2ad503b71c5a4e5bcd8
Mohit <mohitmilindthakur@gmail.com>
2025-10-05 18:12:58 +0530

    2024-4b

--- 2024/day-4/index.js
+++ 2024/day-4/index.js
@@ -8,100 +8,20 @@ for (let lineIndex = 0;  lineIndex < matrix.length; lineIndex++) {
     const line = matrix[lineIndex];
     for (let charIndex = 0; charIndex < matrix[lineIndex].length; charIndex++) {
         const char = matrix[lineIndex][charIndex];
-        if (char !== "X") {
+        if (char !== "A") {
             continue;
         }
-        // HORIZONTAL RIGHT
-        let word = line.substring(charIndex, charIndex + 4);
-        if (word === "XMAS") {
+        const dtl = matrix[lineIndex - 1]?.[charIndex - 1];
+        const dtr = matrix[lineIndex - 1]?.[charIndex + 1];
+        const dbl = matrix[lineIndex + 1]?.[charIndex - 1];
+        const dbr = matrix[lineIndex + 1]?.[charIndex + 1];
+
+        const diag1 = `${dtl}A${dbr}`;
+        const diag2 = `${dtr}A${dbl}`;
+
+        if ((diag1 === "MAS" || diag1 === "SAM") && (diag2 === "MAS" || diag2 === "SAM")) {
             count++;
         }
-
-        // HORIZONTAL LEFT
-        word = line.substring(charIndex - 3, charIndex + 1);
-        console.log(word);
-        if (word === "SAMX") {
-            count++;
-        }
-
-        // VERTICAL TOP
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newLineIndex = lineIndex - i;
-            // if (newLineIndex < 0) break;
-            word += matrix[newLineIndex]?.[charIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
-        // VERTICAL BOTTOM
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newLineIndex = lineIndex + i;
-            // if (newLineIndex > matrix.length - 1) break;
-            word += matrix[newLineIndex]?.[charIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
-        // DIAGONAL TOP LEFT
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newCharIndex = charIndex - i;
-            const newLineIndex = lineIndex - i;
-            // if (newCharIndex < 0 || newLineIndex < 0) {
-            //     break;
-            // }
-            word += matrix[newLineIndex]?.[newCharIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
-        // DIAGONAL TOP RIGHT
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newCharIndex = charIndex + i;
-            const newLineIndex = lineIndex - i;
-            // if (newCharIndex > line.length - 1 || newLineIndex < 0) {
-            //     break;
-            // }
-            word += matrix[newLineIndex]?.[newCharIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
-        // DIAGONAL BOTTOM LEFT
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newCharIndex = charIndex - i;
-            const newLineIndex = lineIndex + i;
-            // if (newCharIndex < 0 || newLineIndex > matrix.length - 1) {
-            //     break;
-            // }
-            word += matrix[newLineIndex]?.[newCharIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
-        // DIAGONAL BOTTOM RIGHT
-        word = "";
-        for (let i = 1; i < 4; i++) {
-            const newCharIndex = charIndex + i;
-            const newLineIndex = lineIndex + i;
-            // if (newCharIndex > line.length - 1 || newLineIndex > matrix.length - 1) {
-            //     break;
-            // }
-            word += matrix[newLineIndex]?.[newCharIndex];
-        }
-        if (word === "MAS") {
-            count++;
-        }
-
     }
 }
 
